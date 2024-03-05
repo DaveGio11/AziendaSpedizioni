3ee3fff78febb7e230f08d3a5f268e71006f95f8
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-03-05 16:52:35 +0100

    Aggiungere i file di progetto.

---
+++ AziendaSpedizioni/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using AziendaSpedizioni.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AziendaSpedizioni.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Utenti u)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionStringDb"].ConnectionString.ToString();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM Utenti WHERE Username = @Username AND Password = @Password";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Username", u.Username);
+                    cmd.Parameters.AddWithValue("@Password", u.Password);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        FormsAuthentication.SetAuthCookie(u.Username, false);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Username o password errati";
+                        return View();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = ex.Message;
+                    return View();
+                }
+                
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CheckCodFiscale(string CodFisc)
+        {
+            string pattern = @"^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(CodFisc))
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult CheckPIVA(string PIVA)
+        {
+            {
+                string pattern = @"^\d{11}$";
+                Regex regex = new Regex(pattern);
+                if (regex.IsMatch(PIVA))
+                {
+                    return Json(true, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(false, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+        DateTime miaData = DateTime.Today;
+        public ActionResult checkDataSpedizione(DateTime data)
+            {
+            DateTime oggi = DateTime.Today;
+            miaData = data;
+            if (data >= oggi)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult checkDataConsegna(DateTime data)
+        {
+            if (data > miaData)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        
+    }
+}
